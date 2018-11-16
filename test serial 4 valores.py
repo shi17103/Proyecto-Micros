f3c4068f8794dc695802a657a36595764c49046d
shi17103 <shi17103@uvg.edu.gt>
2018-11-15 19:50:41 -0600

    Update test serial 4 valores.py

--- test serial 4 valores.py
+++ test serial 4 valores.py
@@ -6,12 +6,12 @@ import sys
 
 ser = serial.Serial(port='COM12',baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS, timeout=0)
 grados = 0
-servo1_rutina = [(0).to_bytes(1, byteorder = 'big')]
-servo2_rutina = [(0).to_bytes(1, byteorder = 'big')]
-servo3_rutina = [(0).to_bytes(1, byteorder = 'big')]
-servo4_rutina = [(0).to_bytes(1, byteorder = 'big')]
+servo1_rutina = [(130).to_bytes(1, byteorder = 'big')]
+servo2_rutina = [(130).to_bytes(1, byteorder = 'big')]
+servo3_rutina = [(130).to_bytes(1, byteorder = 'big')]
+servo4_rutina = [(130).to_bytes(1, byteorder = 'big')]
 
-for x in range(20):
+while (1):
     ser.reset_input_buffer()
     ser.reset_output_buffer()
     time.sleep(.2)
@@ -30,13 +30,10 @@ for x in range(20):
         servo2_rutina.append(servo2_rutina[len(servo2_rutina)-1])
         servo3_rutina.append(servo3_rutina[len(servo3_rutina)-1])
         servo4_rutina.append(servo4_rutina[len(servo4_rutina)-1])
-
-for y in range(1,len(servo1_rutina)):
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-    time.sleep(.2)
-    ser.write(servo1_rutina[y])
-    ser.write(servo2_rutina[y])
-    ser.write(servo3_rutina[y])
-    ser.write(servo4_rutina[y])
-    print(servo1_rutina[y], servo2_rutina[y], servo3_rutina[y], servo4_rutina[y])
+    print(recibido1, recibido2, recibido3, recibido4)
+    ser.write(recibido1)
+    ser.write(recibido2)
+    ser.write(recibido3)
+    ser.write(recibido4)
+    ser.write((13).to_bytes(1, byteorder = 'big'))
+    #print(servo1_rutina[y], servo2_rutina[y], servo3_rutina[y], servo4_rutina[y])
